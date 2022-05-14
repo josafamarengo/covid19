0dd5e2e2098c953ae566ed7c4c333311696e080d
josafamarengo <josafabmarengo@gmail.com>
2022-05-14 14:26:00 -0300

    first commit

---
+++ src/commons/constants/countries.js
@@ -0,0 +1,68 @@
+const COUNTRIES = [
+  {
+    label: 'Mundo',
+    value: 'world',
+  },
+  {
+    label: 'Brasil',
+    value: 'brazil',
+  },
+  {
+    label: 'Alemanha',
+    value: 'germany',
+  },
+  {
+    label: 'Argentina',
+    value: 'argentina',
+  },
+  {
+    label: 'China',
+    value: 'china',  
+  },
+  {
+    label: 'Coréia do Sul',
+    value: 's. korea',
+  },
+  {
+    label: 'Espanha',
+    value: 'spain',
+  },
+  {
+    label: 'Estados Unidos',
+    value: 'usa',
+  },
+  {
+    label: 'França',
+    value: 'france',
+  },
+  {
+    label: 'Índia',
+    value: 'india',
+  },
+  {
+    label: 'Itália',
+    value: 'italy',
+  },
+  {
+    label: 'Japão',
+    value: 'japan',
+  },
+  {
+    label: 'Reino Unido',
+    value: 'uk',
+  },
+  {
+    label: 'Rússia',
+    value: 'russia',
+  },
+  {
+    label: 'Turquia',
+    value: 'turkey',
+  },
+  {
+    label: 'Vietnã',
+    value: 'vietnam',
+  }
+]
+
+export default COUNTRIES
---
+++ src/containers/Main/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const ContainerStyled = styled.div`
+  padding: 20px;
+  margin: 8rem auto;
+  max-width: 1024px;
+  background-color: rgba( 10, 49, 72, 0.9);
+  border-radius: 15px;
+  border: 1px solid white;
+`
+
